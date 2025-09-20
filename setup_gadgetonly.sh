97572198fa09c26fdd72e261bf066c14417d7443
Nguyen Minh Thai <57698887+b4iterdev@users.noreply.github.com>
2025-09-20 15:35:11 +0700

    Update setup_gadgetonly.sh

--- setup_gadgetonly.sh
+++ setup_gadgetonly.sh
@@ -8,7 +8,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 echo_info() {
     echo -e "${GREEN}[INFO] $1${NC}"
@@ -152,5 +152,5 @@ apt update && apt upgrade -y
 
 # Install required packages
 echo "Installing necessary packages..."
-apt install wget gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libcamera libcamera-apps -y
+apt install wget gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libcamera libcamera-apps libmicrohttpd-dev -y
 
